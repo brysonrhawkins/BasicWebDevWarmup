421e950e8b488b5c7ff9c3a42df347a7ac1d7cd4
Bryson Hawkins <64433149+brysonrhawkins@users.noreply.github.com>
2021-02-23 18:00:30 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log("Initialized")
+var user = "Marissa Hawkins";
+var husband = "Bryson Hawkins";
+var anniversary = "10/28/2019";
+
+var relationshipInfo = user + ' was married to ' + husband + ' on ' + anniversary;
+
+console.log(relationshipInfo);
+
+var userAge = 21;
+
+    var moneySaved = true;
+    var ammountSaved = 1000;
+
+if (ammountSaved >=1500){
+    console.log('youre doing great');
+} else{
+    console.log('you can be doing better, ' + user);
+}
+
+
